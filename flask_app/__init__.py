e25a100dbe434cc030902ebc8ed8c35807168853
Jay Patel <jaydpatel99@gmail.com>
2020-10-01 14:18:56 -0400

    added mySQL connection

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import mysql.connector as mysql
+
 app = Flask(__name__)
+
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "P@ssw0rd",
+    database = "flask_app"
+)
+
+cursor = db.cursor()
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello Flask under Apache!'
+    return 'TEST'
 
 if __name__ == "__main__":
     app.run()
+
+
+db.close() 
