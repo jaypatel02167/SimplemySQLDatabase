c911fa89484602c9dbce8c57164561ccffe2fb7e
Jay Patel <jaydpatel99@gmail.com>
2020-10-11 13:52:06 -0400

    react working

---
+++ flask_app/static/react/precache-manifest.607c1916e6cb28078da4c495d8fead48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffc4858a8cbac80b6583239dd6dc78c",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "933f747693ac9e9a4624",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "336b1d4b5bca64a64fdf",
+    "url": "/static/react/js/2.cf1e2948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.cf1e2948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933f747693ac9e9a4624",
+    "url": "/static/react/js/main.e6b51b67.chunk.js"
+  },
+  {
+    "revision": "642bffd405cc52de04d1",
+    "url": "/static/react/js/runtime-main.8fa3ef7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
